bfd68e10512b5fc38d393ee1009ff306631acf9a
Thaylla <142733397+thayllacarolina@users.noreply.github.com>
2023-10-19 21:30:32 -0300

    Home.py

--- Home.py
+++ Home.py
@@ -120,7 +120,7 @@ def map_restaurants(df):
 #-----------------------------------------------------------------------------------------------------------------------------#
 
 ################### importando o dataset
-df = pd.read_csv("../dataset/zomato.csv")
+df = pd.read_csv("dataset/zomato.csv")
 
 ################### limpando o dataset e aplicando as funcoes criadas para preparar o dataset para analise
 df1 = clean_code(df)
